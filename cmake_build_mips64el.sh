2377c0c2d419db58729fd78ca4d19c48775b6b53
itas109 <itas109@qq.com>
2020-12-02 22:14:59 +0800

    [modify] modify mips cross compile info
    Signed-off-by: itas109 <itas109@qq.com>

--- cmake_build_mips64el.sh
+++ cmake_build_mips64el.sh
@@ -4,7 +4,7 @@ mkdir bin_mips64el
 cd bin_mips64el
 
 # ubuntu 20.04
-# sudo apt-get install g++-aarch64-linux-gnu
+# sudo apt-get install g++-mips64el-linux-gnuabi64
 
 cmake -DCMAKE_TOOLCHAIN_FILE=./toolchain_mips64el.cmake -G "Unix Makefiles" -DCOMMLITE_EXE_STATIC=ON ..
 
