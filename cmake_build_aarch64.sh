1517f625bf4063eb225e964b8b249e95551e794e
itas109 <itas109@qq.com>
2020-12-25 23:31:04 +0800

    [modify] update cmake info
    Signed-off-by: itas109 <itas109@qq.com>

--- cmake_build_aarch64.sh
+++ cmake_build_aarch64.sh
@@ -4,7 +4,7 @@ mkdir bin_aarch64
 cd bin_aarch64
 
 # ubuntu 20.04
-# sudo apt-get install g++-aarch64-linux-gn
+# sudo apt-get install g++-aarch64-linux-gnu
 
 cmake -DCMAKE_TOOLCHAIN_FILE=./cmake/toolchain_aarch64.cmake -G "Unix Makefiles" -DCOMMLITE_EXE_STATIC=ON ..
 
